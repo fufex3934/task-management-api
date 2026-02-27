6459f6cbcf6819577e514b1cd5e309a2ed18df81
Fufa Wakgari <fufawakagari174@gmail.com>
2026-02-27 10:23:08 +0300

    connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/task-management'),
+    TasksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './schemas/task.schema';
 
@@ -12,5 +20,28 @@ export class TasksController {
   ): Promise<Task> {
     return await this.tasksService.create(createTaskDto);
   }
-}
 
+  @Get()
+  async getAllTasks(): Promise<Task[]> {
+    return await this.tasksService.findAll();
+  }
+
+  @Get(':id')
+  async getTaskById(@Param('id') id: string): Promise<Task> {
+    return await this.tasksService.findOne(id);
+  }
+
+  @Patch(':id')
+  async updateTask(
+    @Param('id') id: string,
+    @Body()
+    updateData: { title?: string; description?: string; status?: string },
+  ): Promise<Task> {
+    return await this.tasksService.update(id, updateData as Partial<Task>);
+  }
+
+  @Delete(':id')
+  async deleteTask(@Param('id') id: string): Promise<Task> {
+    return await this.tasksService.delete(id);
+  }
+}
