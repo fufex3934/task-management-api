7a8261336e4ea5668406e84f8fba1a0423b0eed2
Fufa Wakgari <fufawakagari174@gmail.com>
2026-03-04 10:16:30 +0300

    configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,37 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import appConfig from './config/app.config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/task-management'),
+    //load configuration based on node_env
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      isGlobal: true, // Makes ConfigService available everywhere
+      load: [appConfig],
+    }),
+    //use configService to get mongo uri
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGODB_URI');
+
+        // Add validation
+        if (!uri) {
+          throw new Error(
+            'MONGODB_URI is not defined in environment variables',
+          );
+        }
+
+        return {
+          uri,
+          maxPoolSize: 10,
+        };
+      },
+      inject: [ConfigService],
+    }),
     TasksModule,
     AuthModule,
     UsersModule,
